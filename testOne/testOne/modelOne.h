71975fd58fe61fc6365ad927d6747b5665319ea4
hongbiny <hongbiny2016@126.com>
2020-09-18 11:49:43 +0800

    创建 modelOne-- 第二次提交

---
+++ testOne/testOne/modelOne.h
@@ -0,0 +1,17 @@
+//
+//  modelOne.h
+//  testOne
+//
+//  Created by hongbiny on 2020/9/18.
+//  Copyright © 2020 DFCA-MAC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface modelOne : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ testOne/testOne/modelOne.m
@@ -0,0 +1,13 @@
+//
+//  modelOne.m
+//  testOne
+//
+//  Created by hongbiny on 2020/9/18.
+//  Copyright © 2020 DFCA-MAC. All rights reserved.
+//
+
+#import "modelOne.h"
+
+@implementation modelOne
+
+@end
