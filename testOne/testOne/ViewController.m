c2473e9f493811983afae1fa96e21907aecec76d
hongbiny <hongbiny2016@126.com>
2020-09-18 14:41:43 +0800

    第五次提交

--- testOne/testOne/ViewController.m
+++ testOne/testOne/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor redColor];
+    self.title = @"titles";
 }
 
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
