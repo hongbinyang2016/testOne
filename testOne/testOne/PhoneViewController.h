a24f43f8e6d28fc26590fd1c7e9eb2fd951d242c
hongbiny <hongbiny20162126.com>
2020-11-14 17:55:24 +0800

    phoneViewController

---
+++ testOne/testOne/PhoneViewController.h
@@ -0,0 +1,17 @@
+//
+//  PhoneViewController.h
+//  testOne
+//
+//  Created by hongbiny on 2020/11/14.
+//  Copyright © 2020 DFCA-MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhoneViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ testOne/testOne/PhoneViewController.m
@@ -0,0 +1,32 @@
+//
+//  PhoneViewController.m
+//  testOne
+//
+//  Created by hongbiny on 2020/11/14.
+//  Copyright © 2020 DFCA-MAC. All rights reserved.
+//
+
+#import "PhoneViewController.h"
+
+@interface PhoneViewController ()
+
+@end
+
+@implementation PhoneViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
